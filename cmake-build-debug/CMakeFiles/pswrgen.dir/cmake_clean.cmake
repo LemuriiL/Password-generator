3301e2e7f573180aaa035060ca1cebf016114b1d
neonovyj <vx200014@gmail.com>
2021-10-28 14:47:09 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/pswrgen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pswrgen.dir/main.cpp.o"
+  "pswrgen"
+  "pswrgen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pswrgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pswrgen.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pswrgen.
+# This may be replaced when dependencies are built.
